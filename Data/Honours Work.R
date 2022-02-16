115f98c00009d15daa724daf935db73dd24649a1
Deji <ashote@dundee.ac.uk>
2022-02-16 14:23:51 +0000

    Reworked a lot of my methods as i needed to find the total population of the countries at the time in order to add the percentages properly

--- Data/Honours Work.R
+++ Data/Honours Work.R
@@ -1,26 +1,38 @@
 install.packages("ggplot2")
 install.packages("dplyr")
+install.packages("tidyr")
+install.packages("daff")
+install.packages("sqldf")
 library(ggplot2)
+library(tidyr)
 library(dplyr)
 library(readr)
+library(daff)
+library(sqldf)
 fooddata <- read_csv("Data/dietary-compositions-by-commodity-group.csv")
 ObesityData <- read_csv("C:/Users/ayode/Music/HonoursProject/Data/share-of-adults-who-are-overweight.csv")
+worldPop <- read_csv("Data/historic-and-un-pop-projections-by-age.csv")
 ObesityData <- ObesityData %>% mutate(Entity = factor(Entity))
-View(ObesityData)
-View(fooddata)
+#This is the dataframe of the countries with their percentage of the population that is obese. 
 attach(ObesityData)
 ObesityData%>%
   select(Entity,Overweight,Code)%>%
   filter(Code !="NA") %>%
   group_by(Entity) %>%
   summarise(Total = sum(Overweight))
-
-properdata<-ObesityData%>%
-  select(Entity,Overweight,Code,Year)%>%
+attach(worldPop)
+#This is the dataframe of the countries with their year and adult population
+TPop <-worldPop%>%
+  select(Entity,Code,Year,Total,`under age 15`)%>%
   filter(Code !="NA") %>%
-  group_by(Year) %>%
-  summarise(sum(Overweight/42))
-
+  filter(Year>1974 & Year<2016) %>%
+  group_by(Year,Entity) %>%
+  summarise(Totalpop = Total-`under age 15`)
+# This is the dataframe of the countries with their total weight
+properdata<-ObesityData%>%
+  filter(Code !="NA") %>%
+  select(Entity,Overweight,Year)%>%
+  group_by(Year,Entity)
 
 ObesityData %>% 
   filter(Entity =='Afghanistan')  %>% 
@@ -29,3 +41,15 @@ ObesityData %>%
   geom_smooth(method=lm)
 
 summary(lm(Overweight ~ year))
+
+sorted<-merge(properdata,Temppop)
+rm(Temppop)
+
+
+
+
+
+
+
+
+
